731184ca603150b3eaf992a92545fdf4e88e7b1b
Andrew Yee <andrewyee18331@sbcglobal.net>
2017-01-07 23:15:31 -0800

    v1.0
    weekend work to have my first version of my “resume site”

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,49 +1,35 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Andrew Niko Yee</title>
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <link href="/public/css/main.css" rel="stylesheet" type="txt/css">
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Lato:100,300" rel="stylesheet" type="text/css">
         <style>
-            /* ----------- iPhone 6 ----------- */
-
-            /* Portrait and Landscape */
-            @media only screen 
-              and (min-device-width: 375px) 
-              and (max-device-width: 667px) 
-              and (-webkit-min-device-pixel-ratio: 2) { 
-
-            }
-
-            /* Portrait */
-            @media only screen 
-              and (min-device-width: 375px) 
-              and (max-device-width: 667px) 
-              and (-webkit-min-device-pixel-ratio: 2)
-              and (orientation: portrait) { 
-                  .center{
-                      width: 90% !important;
-                      height: 90% !important;
-                      max-width: none !important;
-                  }
-            }
-
-            /* Landscape */
-            @media only screen 
-              and (min-device-width: 375px) 
-              and (max-device-width: 667px) 
-              and (-webkit-min-device-pixel-ratio: 2)
-              and (orientation: landscape) { 
-
-            }
-            
             body{
                 padding: 20px;
-                background: linear-gradient(
-                    rgba(145, 186, 206, 0.25), 
-                    rgba(145, 186, 206, 0.25)),
-                    url('/public/img/beach.jpg') no-repeat center center fixed; 
+                background: -webkit-linear-gradient(left,
+                    rgba(178, 215, 174, 0.5), 
+                    rgba(125, 108, 192, 0.5)), 
+                    url('/public/img/winter.jpg') no-repeat center center fixed;
+                
+                background: -o-linear-gradient(right,
+                    rgba(178, 215, 174, 0.5), 
+                    rgba(125, 108, 192, 0.5)),
+                    url('/public/img/winter.jpg') no-repeat center center fixed;
+                
+                background: -moz-linear-gradient(right,
+                    rgba(178, 215, 174, 0.5), 
+                    rgba(125, 108, 192, 0.5)), 
+                    url('/public/img/winter.jpg') no-repeat center center fixed;
+                
+                background: linear-gradient(to right,
+                    rgba(178, 215, 174, 0.5), 
+                    rgba(125, 108, 192, 0.5)),
+                    url('/public/img/winter.jpg') no-repeat center center fixed;
+                
                 -webkit-background-size: cover;
                 -moz-background-size: cover;
                 -o-background-size: cover;
@@ -51,33 +37,25 @@
                 font-weight: 100;
                 font-family: 'Lato';
             }
-            .main{
-
-            }
-            .row {
-                
+            @media screen and (orientation : portrait)
+            { 
+                body{
+                    font-weight: 400;
+                }
             }
             .center{
-                background: rgba(19, 27, 39, 0.6);
+                background: rgba(20, 30, 40, 0.8);
                 color: white;
-                height: 800px;
-                width: 75%;
-                max-width: 500px;
-                transform: translate(-50%, -50%);
-                position: absolute;
-                top: 50%;
-                left: 50%;
                 padding: 20px;
-                overflow: auto;
                 border-radius: 5px;
-                
             }
-            .title {
-                font-size: 50px;
-                margin: 0px 0px 20px 0px;
+            a{
+                color: #8ffeb3;
+            }
+            img{
+                height: 300px;
             }
             .sub-title {
-                font-size: 24px;
                 text-align: left;
                 padding-left: 40px;
             }
@@ -93,55 +71,61 @@
                 margin: 10px;
                 width: 100px;
                 text-align: center;
+                background: -webkit-linear-gradient(left, rgb(34, 113, 101) , rgb(30, 87, 143));
+                background: -o-linear-gradient(right, rgb(34, 113, 101), rgb(30, 87, 143));
+                background: -moz-linear-gradient(right, rgb(34, 113, 101), rgb(30, 87, 143));
+                background: linear-gradient(to right, rgb(34, 113, 101) ,rgb(30, 87, 143));
             }
-            .title-box {
-                margin: 15px 35px 35px 35px;
-                padding: 0px 18px;
+            address > .glyphicon {
+                margin-right: 8px;
             }
-            .title-box-img {
-                height: 150px;
-                width: 150px;
-                display: inline-block;
-            }
-            .title-box-img img{
-                width: 100%;
-                height: 100%;
-                border-radius: 50%;
-            }
-            .title-box-info{
-                margin: 0px 15px;
-                display: inline-block;
-                vertical-align: middle;
-            }
-            .title-box-info .name{
-                font-size: 40px;
-            }
-            .title-box-info .text{
-                font-size: 15px;
+            .h-divider{
+                border-bottom: 2px solid white;
+                padding: 12px;
             }
         </style>
     </head>
     <body>
-        <div class="main">
-            <div class="center">
+        <div class="row">
+            <div class="center col-xs-12 col-sm-12 col-md-4 col-md-offset-4 col-lg-4 col-lg-offset-4">
                 <div class="row">
-                    <div class="title-box">
-                        <div class="title-box-img">
-                            <img src="https://scontent.fsnc1-5.fna.fbcdn.net/v/t1.0-9/15578602_10154763017469400_2557445869749072963_n.jpg?oh=2e9bb72c6d1be42d6dafaabd4a4b34f8&oe=58D9C3C9">
-                        </div>
-                        <div class="title-box-info">
-                            <div class="name">
-                                Andrew Yee
-                            </div>
-                            <div class="text">
-                                <span class="glyphicon glyphicon-earphone" aria-hidden="true"></span>  (818) 731-8139 <br>
-                                <span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>  andrew.yee@mail.com
-                            </div>
-                        </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="text-align: center;">
+                        <img src="/public/img/selfie-m.jpg" alt="selfie" class="img-circle">
                     </div>
                     
+                    <div class="text-center col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                        <h1>Andrew Yee</h1>
+                        <address>
+                            <span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>
+                            <a href="mailto:#">andrew.yee@mail.com</a>
+                            <br>
+                            <span class="glyphicon glyphicon-earphone" aria-hidden="true"></span>
+                            <a href="#">(818) 731-8139</a>
+                        </address>
+                    </div>
+
+                    <div class="h-divider col-xs-8 col-md-8 col-sm-8 col-md-offset-2 col-xs-offset-2 col-sm-offset-2"></div>
+
                     <div class="sub-title col-xs-12 col-md-12 col-sm-12">
-                        Languages:
+                        <h3>About</h3>
+                    </div>
+
+                    <div class="sub-title-list col-xs-12 col-md-12 col-sm-12">
+                        <p>
+                            I am a 2014 CSUEB graduate with a major in 
+                            Computer Science and Engineering with a minor in 
+                            Mathematics. From 2012-14 I worked as a student 
+                            assistant for CSUEB Web Service department. In 2015,   
+                            I worked as a Software Engineer for a YC startup 
+                            company and ever since the beginning of 2016 I am 
+                            a Ruby on Rails developer for Metaformers.
+                        </p>
+                    </div>
+                    
+                    <div class="h-divider col-xs-8 col-md-8 col-sm-8 col-md-offset-2 col-xs-offset-2 col-sm-offset-2"></div>
+
+                    <div class="sub-title col-xs-12 col-md-12 col-sm-12">
+                        <h3>Languages</h3>
                     </div>
                     <div class="sub-title-list col-xs-12 col-md-12 col-sm-12">
                         <span class="sp-list">Ruby</span>
@@ -154,8 +138,10 @@
                         <span class="sp-list">Java</span>
                     </div>
                     
+                    <div class="h-divider col-xs-8 col-md-8 col-sm-8 col-md-offset-2 col-xs-offset-2 col-sm-offset-2"></div>
+
                     <div class="sub-title col-xs-12 col-md-12 col-sm-12">
-                        Frameworks:
+                        <h3>Frameworks</h3>
                     </div>
                     <div class="sub-title-list col-xs-12 col-md-12 col-sm-12">
                         <span class="sp-list">Rails</span>
@@ -167,7 +153,6 @@
                         <span class="sp-list">Bootstrap</span>
                         <span class="sp-list">Foundation</span>
                     </div>
-                    
                 </div>
             </div>
         </div>
